6615ac739ba6a0bd52e47cf7927cbd95ad10c11b
Artem Hordiyevskyy <artem2500@gmail.com>
2023-12-28 19:17:54 +0200

    aa

--- src/js/localStorageApi.js
+++ src/js/localStorageApi.js
@@ -40,11 +40,9 @@ localStorageApi.deleteCart() - removes cart from storage
 
 */
 
-import { refreshIcons } from "./discount";
-
 export default {
-  _filterID: "filter-project-Bpfbt9js",
-  _cartID: "cart-project-Bpfbt9js",
+  _filterID: 'filter-project-Bpfbt9js',
+  _cartID: 'cart-project-Bpfbt9js',
   _save: function _save(key, data) {
     try {
       const jsonData = JSON.stringify(data);
@@ -85,7 +83,6 @@ export default {
   },
   saveCart: function saveCart(cart) {
     this._save(this._cartID, cart);
-    refreshIcons("discountIcon");
   },
   loadCart: function loadCart() {
     let cart = this._load(this._cartID);
--- src/js/renderProductList.js
+++ src/js/renderProductList.js
@@ -1,43 +1,36 @@
-import fetchAPI from "./fetchApi.js";
-import localStorageApi from "./localStorageApi.js";
+import fetchAPI from './fetchApi.js';
 
-import discountIcon from "../img/icons.svg#discount-icon";
-import cartIcon from "../img/icons.svg#discount-cart";
-import checkedIcon from "../img/icons.svg#discount-checked";
+import { getProductModal } from './modalProduct.js';
 
-const productsList = document.querySelector(".product-card-list");
+
+const productsList = document.querySelector('.product-card-list');
+const productListApi = await fetchAPI.products();
 
 function onCardClick(e) {
-  const cardId = e.target.closest(".product-card-item").id;
+  // const cardId = e.target.closest('.product-card-item').id;
 
-  if (
-    e.target.classList.contains("product-card-item") ||
-    e.target !== e.currentTarget
-  ) {
-    if (
-      e.target.nodeName === "use" ||
-      e.target.nodeName === "BUTTON" ||
-      e.target.nodeName === "svg"
-    ) {
-      console.log("Buy prod", cardId);
-
-      return;
+  if (e.target.classList.contains('product-card-item') || e.target !== e.currentTarget) {
+    if (e.target.nodeName === "use" || e.target.nodeName === "BUTTON" || e.target.nodeName === "svg") {
+  
+      console.log('add prod')
+     
+   return;
     }
-
-    console.log("Open modal", cardId);
-
-    return;
+  
+    getProductModal(e, '.product-card-item');
+    return
   }
+  
 }
 function handleMarkup(data) {
-  const { category, img, name, popularity, price, size, _id, is10PercentOff } =
-    data;
+  const { category, img, name, popularity, price, size, _id, is10PercentOff } = data;
 
-  const markup = `
-  <li class="product-card-item discount-list-item" id=${_id} data-discount="${is10PercentOff}">
+  if (is10PercentOff) {
+    return `
+  <li class="product-card-item discount-list-item" id=${_id} data-productId="${_id}" >
 
   <svg class="discount-icon" id=${_id}>
-   <use href="${discountIcon}"></use>
+   <use href="icon.svg#discount-icon"></use>
  </svg>
        <div class="product-card-wrapper">
         <div class="card-img-wrapper"><img src="${img}" alt="${name}" class="product-card-image"></div>
@@ -57,51 +50,68 @@ function handleMarkup(data) {
         <p class="product-curd-price">$${price}</p>
         <button class="card_buy-btn">
           <svg class="card_buy-logo-icon">
-                <use href="${cartIcon}"></use>
+                <use href="./img/icons.svg#shopping-cart-icon"></use>
+          </svg>
+        </button>
+        </div>
+        </div>
+    </li>
+  `
+  }
+  
+  return `
+  <li class="product-card-item discount-list-item" id=${_id} data-productId="${_id}" >
+
+       <div class="product-card-wrapper">
+        <div class="card-img-wrapper"><img src="${img}" alt="${name}" class="product-card-image"></div>
+        <h2 class="product-card-name">${name}</h2>
+        <div class="product-card-info">
+            <p class="info-text">
+                Category:<span class="info-span">${category}</span>
+            </p>
+            <p class="info-text">
+                Size:<span class="info-span">${size}</span>
+            </p>
+            <p class="info-text">
+                Popularity:<span class="info-span">${popularity}</span>
+            </p>
+        </div>
+        <div class="product-card-bottom">
+        <p class="product-curd-price">$${price}</p>
+        <button class="card_buy-btn">
+          <svg class="card_buy-logo-icon">
+                <use href="./img/icons.svg#shopping-cart-icon"></use>
           </svg>
         </button>
         </div>
         </div>
     </li>
   `;
+  
+};
 
-  return markup;
-}
+function renderCards() {
+  productListApi.results.map(item => {
+    return productsList.insertAdjacentHTML('beforeend', handleMarkup(item));
+  }
+  )
+};
 
-async function renderCards() {
-  const productListApi = await fetchAPI.products();
-
-  productListApi.results.map((item) => {
-    return productsList.insertAdjacentHTML("beforeend", handleMarkup(item));
-  });
-}
-
-function shownDiscIcon(arr) {
-  arr.map((el) => {
-    const icon = document.querySelector(".discount-icon");
-
-    console.log(icon);
-  });
-}
-// shownDiscIcon(productListApi.results)
 
 function renderSearchedCards(category, search) {
-  productsList.innerHTML = "";
+  productsList.innerHTML = '';
 
   if (category) {
-    return category.map((item) =>
-      productsList.insertAdjacentHTML("beforeend", handleMarkup(item))
-    );
+    return category.map(item => productsList.insertAdjacentHTML('beforeend', handleMarkup(item)));
   }
 
   if (search) {
     if (search.length === 0) {
-      window.alert("Oops test");
-    }
-    return search.map((item) =>
-      productsList.insertAdjacentHTML("beforeend", handleMarkup(item))
-    );
+        window.alert('Oops something went wrong')
+      }
+    return search.map(item => productsList.insertAdjacentHTML('beforeend', handleMarkup(item)));
   }
-}
+};
 
-export { onCardClick, renderCards, renderSearchedCards, productsList };
+export{onCardClick, renderCards, renderSearchedCards, productsList};
+
